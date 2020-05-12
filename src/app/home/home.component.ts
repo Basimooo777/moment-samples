2122060438d1738c0b7b60c5c566b264fb978199
absoliman <absoliman@ejada.com>
2020-05-13 01:54:39 +0300

    finalize the sample

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,27 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import * as hijri from 'moment-hijri';
-import * as moment from 'moment';
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  myDate;
-  myDate1;
-  today;
-  myDate2;
-
-  constructor() { }
+  date: string;
+  convertedDate: string;
 
   ngOnInit(): void {
-    this.myDate  = hijri().format('iYYYY/iM/iD'); // today
+    this.date = '';
+  }
 
-    this.myDate1  = hijri('2014-11-28 16:40:00', 'YYYY-M-D HH:mm:ss').endOf('iMonth').format('iYYYY/iM/iD HH:mm:ss');
-
-    this.today = hijri().format('YYYY-MM-DD'); // today
-
-    this.myDate2  = hijri(this.today, 'YYYY-M-D').endOf('iMonth').format('iYYYY/iM/iD'); // today
- }
-
+  convertDate(f: NgForm): void {
+    this.convertedDate = hijri(f.value.date, 'D-M-YYYY').locale('en').format('iD-iM-iYYYY');
+    return;
+  }
 }
